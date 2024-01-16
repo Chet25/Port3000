ab35c0a8cd16f37690cac2cf901e1cf14e86b95d
Chet25 <chetan25december@gmail.com>
2024-01-16 22:32:45 +0530

    first commit

---
+++ js/main.js
@@ -0,0 +1,28 @@
+
+document.querySelector('.first-puzzle').addEventListener('click',first)
+document.querySelector('.second-puzzle').addEventListener('click',second)
+document.querySelector('.third-puzzle').addEventListener('click',third)
+document.querySelector('.fourth-puzzle').addEventListener('click',fourth)
+
+
+
+function first(){
+    window.location.href= "#"
+    console.log('Home');
+
+}
+function second(){
+    window.location.href= "#section2"
+    console.log('About');
+
+}
+function third(){
+    window.location.href= "#section3"
+    console.log('work');
+
+}
+function fourth(){
+    window.location.href= "#"
+    console.log('contacts');
+
+}
